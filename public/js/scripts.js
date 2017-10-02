a021d464c733fdacb37cc3e237b7a765155660df
sljohnson32 <sljohnson32@gmail.com>
2017-10-02 15:03:07 -0600

    Initial commit

---
+++ public/js/scripts.js
@@ -0,0 +1,25 @@
+
+function showDropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function selectProject() {
+    console.log('PROJECT SELECTED')
+}
+
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
