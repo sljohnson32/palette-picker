f156b68967a54423cfc76d49f4ecc424c2dc0009
sljohnson32 <sljohnson32@gmail.com>
2017-10-03 18:16:07 -0600

    Add initial logic for loading saved palettes and minimal css

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -65,26 +65,49 @@ const populateProjects = () => {
   projects.forEach(project => {
     let listHTML = `<a onclick="selectDropdownProject()">${project}</a>`;
     let projectHTML = getProjectHTML(project);
-    let palatteHTML = populateProjectPalettes(project)
+    let paletteHTML = populateProjectPalettes(project)
     $('#myDropdown').append(listHTML);
     $('.saved-palette-container').append(projectHTML)
+    $('.palette-container:last').append(paletteHTML)
   })
 }
 
 const getProjectHTML = (project) => {
   let html = `<div class='project-container' onclick='selectProject()'>
                 <h2>${project}</h2>
-                <div class=${project}>
+                <div class='palette-container'>
                 </div>
               </div>`;
   return html;
 }
 
 const populateProjectPalettes = (project) => {
-  let paletteHTML = palettes.filter((palette) => {
+  let paletteHTML = '';
+  let projectPalette = palettes.filter((palette) => {
     return palette.project == project
   })
-  console.log(project, paletteHTML)
+  projectPalette.forEach(palette => {
+    paletteHTML = paletteHTML.concat(generateSavedPalette(palette.name, palette.colors));
+  })
+  return paletteHTML;
+}
+
+const generateSavedPalette = (name, colors) => {
+  let html = `
+    <div class="saved-palette">
+      <h3>${name}</h3>
+      <ul class="color-box-container">
+        <li class="color-box" style="background-color:${colors[0]}"/>
+        <li class="color-box" style="background-color:${colors[1]}"/>
+        <li class="color-box" style="background-color:${colors[2]}"/>
+        <li class="color-box" style="background-color:${colors[3]}"/>
+        <li class="color-box" style="background-color:${colors[4]}"/>
+        <li class="color-box" style="background-color:${colors[5]}"/>
+      </ul>
+      <img src='./imgs/trash-bin.png' />
+    </div>
+  `
+  return html;
 }
 
 // Close the dropdown menu if the user clicks outside of it
