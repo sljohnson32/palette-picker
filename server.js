76320afb7a44635eb0d366b759aa5f6df95bfe56
sljohnson32 <sljohnson32@gmail.com>
2017-10-04 16:23:13 -0600

    Add body parse lib and POST endpoints for projects and palettes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
@@ -31,8 +34,43 @@ app.get('/api/v1/palettes', (request, response) => {
     });
 });
 
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
 
+  if (!project.name) {
+    return response.status(422)
+      .send({ error: "Expected format: { name: <String> }. You're missing the name property."});
+  }
 
+  database('projects').insert(project, 'id')
+    .then(project => {
+      response.status(201).json({ id: project[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
+  const paletteFields = ['name', 'color_1', 'color_2', 'color_3', 'color_4', 'color_5', 'color_6', 'project_id']
+
+  for (let requiredParameter of paletteFields) {
+    if (!palette[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, color_1: <String>, color_2: <String>, color_3: <String>, color_4: <String>, color_5: <String>, color_6: <String>, project_id: <Integer> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json({ id: palette[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
 
 app.listen(3000, () => {
   console.log('Projects server running on localhost:3000')
