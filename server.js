85dd8d36b84c70905b99b060acb92678956c1619
sljohnson32 <sljohnson32@gmail.com>
2017-10-04 15:43:04 -0600

    project and palette get endpoints created

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+  response.sendfile('index.html');
+})
+
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then((projects) => {
+      response.status(200).json({ projects })
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    });
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then((palettes) => {
+      response.status(200).json({ palettes })
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Projects server running on localhost:3000')
+})
