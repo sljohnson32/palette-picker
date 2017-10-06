045a977bb07413e0c03d9fe2c51105a46aff11ff
sljohnson32 <sljohnson32@gmail.com>
2017-10-06 10:34:05 -0600

    Add dynamic port setting

--- server.js
+++ server.js
@@ -120,8 +120,8 @@ app.delete('/api/v1/palettes/:id', (request, response) => {
 
 })
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.get('env')} server is running on localhost:${app.get('port')}.`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 module.exports = app;
