2dbf201acd67419a049dd09921308c593abe7c38
sljohnson32 <sljohnson32@gmail.com>
2017-10-06 10:41:47 -0600

    Another port fix

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.delete('/api/v1/palettes/:id', (request, response) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
 });
 
 module.exports = app;
